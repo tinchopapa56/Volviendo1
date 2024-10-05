4b1ab3730d551224c3580903c4befd749c993787
Tincho Develops <97058453+tinchopapa56@users.noreply.github.com>
2024-10-05 12:17:52 -0300

    header donw - will use my own styling stuff

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,7 +1,141 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const Header = () => {
-  return <div>Header</div>;
+const Header = ({
+  account,
+  connectWallet,
+  setAccount,
+  setIsLoading,
+  setOwnerModal,
+  shortAddress,
+  tokenDetails,
+  currency,
+  ownerModal,
+}) => {
+
+  const [isMetamaskInstalled, setIsMetamaskInstalled] = useState(false)
+
+  useEffect(() => {
+    const metamaskInstalled = window.ethereum
+    if (metamaskInstalled) {
+      setIsMetamaskInstalled(true)
+
+      window.ethereum.on("accountsChanged", handleAccountsChanged)
+    }
+
+    return () => {
+      if (window.ethereum)
+        window.ethereum.removeListener("accountsChanged", handleAccountsChanged)
+    }
+  }, [])
+
+  const handleAccountsChanged = accounts => {
+    setAccount(accounts[0])
+  }
+
+  const connectMetamask = async () => {
+    if (typeof window.ethereum !== undefined) {
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccouns",
+        })
+        setAccount(accounts[0])
+      } catch (err) {
+        console.log(err)
+      }
+    } else {
+      console.log("install metamask")
+    }
+  }
+
+  const headerClass = {
+    display: "flex",
+    justifyContent: "space-between",
+    padding: "0 1em"
+  };
+
+  return (
+    <header className="site-header header--transparent ico-header">
+      <div className="header__main-wrap">
+        <div className="header__main-wrap">
+          <div
+            className="header__main-wrap ul_li_bewtween"
+            style={headerClass}
+          >
+            <div className="header__left ul_li">
+              <div className="header__logo">
+                <a href="/">
+                  <img src="assets/img/logo/logo.svg" alt="" srcset="" />
+                </a>
+              </div>
+            </div>
+            <div className="main-menu--wrap ul_li navbar navbar-expand-xl" >
+              <nav className="main-menu collapse navbar-collapse" >
+                <ul>
+                  <li className="active has-mega-menu">
+                    <a href="/">Home</a>
+                  </li>
+                  <li>
+                    <a className="scrollspy-btn" href="#about">About</a>
+                  </li>
+                  <li>
+                    <a className="scrollspy-btn" href="#roadmap">Roadmap</a>
+                  </li>
+                  <li>
+                    <a className="scrollspy-btn" href="#team">Team</a>
+                  </li>
+                  <li>
+                    <a className="scrollspy-btn" href="#FAQ">FAQ</a>
+                  </li>
+                  <li>
+                    <a className="scrollspy-btn" href="#contact">Contact</a>
+                  </li>
+                  <li>
+                    <a
+                      onClick={() => ownerModal ? setOwnerModal(false) : setOwnerModal(true)}
+                      style={{ cursor: "pointer" }}
+                    >
+                      Tools
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+
+            <div className="header__action ul_li">
+              <div className="d-xl-none">
+                <a className="header_bar_hamburger_menu"></a>
+                <div className="header__bar-icon">
+                  <span />
+                  <span />
+                  <span />
+                  <span />
+                </div>
+
+              </div>
+              {account ? (
+                <div className="header__account">
+                  <a onClick={() => navigator.clipboard.writeText(tokenDetails.address)}>
+                    {shortAddress(tokenDetails?.address) ?? ""}
+                    {tokenDetails?.maticBal?.slice(0, 6)}
+                    {currency}
+                  </a>
+                </div>
+              ) : (
+                <div className="header__account">
+                  <button style={{backgroundColor:"#966ef1", color:"white"}} onClick={() => connectMetamask()}>
+                    Connect Wallet
+                  </button>
+                </div>
+              )}
+            </div>
+
+
+          </div>
+        </div>
+      </div>
+    </header>
+
+  )
+
 };
-
 export default Header;
